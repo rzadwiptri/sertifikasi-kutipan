2e24f575ba455ed06b9d6151256593559367beed
rzadwiptri <reza.210170061@mhs.unimal.ac.id>
2023-11-22 21:31:25 +0700

    Pesan commit Anda di sini

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from pymongo import MongoClient
 import jwt
 from datetime import datetime, timedelta
@@ -7,29 +9,36 @@ from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from bson import ObjectId 
 
-app = Flask(__name__)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "./static/profile_pics"
 app.config["SECRET_KEY"] = "SPARTA"
 app.config["TOKEN_KEY"] = "mytoken"
 
-cxn_str = 'mongodb+srv://rzadwiptri:kelompok5@cluster0.sbzask3.mongodb.net/'
-client = MongoClient(cxn_str)
 
-db = client.db_sertifikasi
 
 @app.route('/')
 def index():
+    nama_lengkap = "REZA DWI PUTRI" 
+    nama = f"Selamat datang di kutipan {nama_lengkap}, silakan isi kutipan Anda."
     kutipan = db.kutipan.find()
-    return render_template('index.html', kutipan=kutipan)
+    return render_template('index.html', kutipan=kutipan, nama=nama)
 
 @app.route('/add_quote', methods=['POST'])
 def add_quote():
     if request.method == 'POST':
-        kutipan = request.form['kutipan']
+        kutipan = request.form['kutipan'] 
         now = datetime.now() 
         date_time = now.strftime("%Y-%m-%d %H:%M:%S")
-        db.kutipan.insert_one({'quote': kutipan, 'created_at': date_time })
+        db.kutipan.insert_one({'quote': kutipan, 'created_at': date_time})
         return redirect(url_for('index'))
     
 
@@ -48,7 +57,7 @@ def upload_file():
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
                 return f'File {filename} berhasil diunggah'
     
-    return 'Tidak ada file yang diunggah atau terjadi kesalahan.'  
+    return 'Tidak ada file yang diunggah atau terjadi kesalahan.' 
 
 @app.route('/detail', methods=['POST'])
 def detail():
